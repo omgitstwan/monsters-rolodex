d61258a3d1f06eff3b0b2807e35f6d918a5735bb
omgitstwan <46748974+omgitstwan@users.noreply.github.com>
2019-12-03 17:34:45 +0100

    Updates

---
+++ precache-manifest.17ce2e0416dc0d2b49a8ac43ca0a811e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "981248becba004c68675e4f62aa45a59",
+    "url": "/monsters-rolodex-course/index.html"
+  },
+  {
+    "revision": "820e2cb9e60196f82acf",
+    "url": "/monsters-rolodex-course/static/css/main.7aa0a2cd.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex-course/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "820e2cb9e60196f82acf",
+    "url": "/monsters-rolodex-course/static/js/main.1213c878.chunk.js"
+  },
+  {
+    "revision": "4563866542b34e634f28",
+    "url": "/monsters-rolodex-course/static/js/runtime-main.806c35f1.js"
+  }
+]);
